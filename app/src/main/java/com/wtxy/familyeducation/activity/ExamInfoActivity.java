3769d24898d0f736d8980155d34454aab17aac07
hxy <hxy177928@alibaba-inc.com>
2020-05-28 19:28:24 +0800

    update

--- app/src/main/java/com/wtxy/familyeducation/activity/ExamInfoActivity.java
+++ app/src/main/java/com/wtxy/familyeducation/activity/ExamInfoActivity.java
@@ -158,7 +158,8 @@ public class ExamInfoActivity extends BaseActivity {
         String examName = edtTitle.getText().toString().trim();//取考试名称
         String subjectName = tv_subject.getText().toString().trim();//取考试科目
         String className = tvClass.getText().toString().trim();//取班级名称
-        mGradeInfo.exam_name = examName;
+        mGradeInfo.exam_name = examName;//赋值给考试对象，用于提交(其它信息都在onActivityResult方法中赋值过了，具体看onActivityResult方法)
+        //如果哪一项为空，toast提示一下
         if (TextUtils.isEmpty(examName) || TextUtils.isEmpty(subjectName) || TextUtils.isEmpty(className)) {
             Toast.makeText(this, "请输入或选择对应的信息", Toast.LENGTH_SHORT).show();
             return;
@@ -182,26 +183,26 @@ public class ExamInfoActivity extends BaseActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {//从别的页面返回触发这个方法
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 2000 && resultCode == 2000) {
+        if (requestCode == 2000 && resultCode == 2000) {//从选择班级页面回来，取一下班级信息
             ClassInfo classInfo = (ClassInfo) data.getSerializableExtra("classInfo");
             if (classInfo != null && mGradeInfo != null) {
-                mGradeInfo.class_id = classInfo.getClass_id();
+                mGradeInfo.class_id = classInfo.getClass_id();//取班级id和名称，赋值给考试对象，用于保存上传
                 mGradeInfo.class_name = classInfo.getClass_name();
-                tvClass.setText(mGradeInfo.class_name);
+                tvClass.setText(mGradeInfo.class_name);//当前页面上的班级信息也显示上
             }
-        } else if (requestCode == 1000 && resultCode == 1) {
+        } else if (requestCode == 1000 && resultCode == 1) {//从选择科目页面回来，取一下科目信息
             SubjectInfo subjectInfo = (SubjectInfo) data.getSerializableExtra(Const.KEY_SUBJECT_INFO);
             if (subjectInfo != null && mGradeInfo != null) {
-                mGradeInfo.subject_id = subjectInfo.getSubject_id();
+                mGradeInfo.subject_id = subjectInfo.getSubject_id();//取科目id和名称，赋值给考试对象，用于保存上传
                 mGradeInfo.subject_name = subjectInfo.getSubject_name();
-                tv_subject.setText(mGradeInfo.subject_name);
+                tv_subject.setText(mGradeInfo.subject_name);//当前页面上的科目信息也显示上
             }
         }
     }
 
-    private void getTestScore() {
+    private void getTestScore() {//接口没好时候的测试数据
         NameFactory nameFactory = new NameFactory();
         for (int i = 0; i < 10; i++) {
             ScoreInfo score = new ScoreInfo();
@@ -212,39 +213,39 @@ public class ExamInfoActivity extends BaseActivity {
         }
     }
 
-    private void setScoreInfo(StudentInfo studentInfo) {
+    private void setScoreInfo(StudentInfo studentInfo) {//给班级学生列表赋值考试信息，有就显示分数，没有就不显示
         if (scoreList.size() > 0) {
-            for (ScoreInfo scoreInfo : scoreList) {
-                if (scoreInfo.student_id == studentInfo.student_id) {
-                    studentInfo.scoreInfo = scoreInfo;
+            for (ScoreInfo scoreInfo : scoreList) {//for循环分数列表的每一项
+                if (scoreInfo.student_id == studentInfo.student_id) {//如果某个分数信息中的学生id和参数传进来的学生信息的id相同，则取学生的分数
+                    studentInfo.scoreInfo = scoreInfo;//讲分数对象赋值给当前学生
                     return;
                 }
             }
         }
     }
 
-    private void addScoreView() {
+    private void addScoreView() {//添加学生列表页，只有不是新建考试信息的时候才有
         LayoutInflater inflate = LayoutInflater.from(this);
-        llGrade.removeAllViews();
-        for (final StudentInfo studentInfo : studentInfoList) {
-            View view = inflate.inflate(R.layout.layout_score_item, null);
-            TextView tvName = view.findViewById(R.id.tv_name);
-            TextView tvScore = view.findViewById(R.id.tv_score);
-            tvName.setText(studentInfo.student_name);
-            if (studentInfo.scoreInfo != null) {
+        llGrade.removeAllViews();//清空学生列表页上其它多余view，重新添加
+        for (final StudentInfo studentInfo : studentInfoList) {//for循环每个学生，都创建一行数据
+            View view = inflate.inflate(R.layout.layout_score_item, null);//新建一行
+            TextView tvName = view.findViewById(R.id.tv_name);//绑定这一行上的学生姓名控件
+            TextView tvScore = view.findViewById(R.id.tv_score);//绑定这一行上的学生分数控件
+            tvName.setText(studentInfo.student_name);//先把学生姓名显示上
+            if (studentInfo.scoreInfo != null) {//如果学生的分数信息不为空，则显示分数
                 if ((studentInfo.scoreInfo.score_num + "").endsWith(".0")) {
                     tvScore.setText((int) studentInfo.scoreInfo.score_num + "");
                 } else {
                     tvScore.setText(studentInfo.scoreInfo.score_num + "");
                 }
-            } else {
+            } else {//否则如果学生的分数信息为空的话，分数控件隐藏
                 tvScore.setVisibility(View.GONE);
             }
-            llGrade.addView(view);
-            view.setOnClickListener(new View.OnClickListener() {
+            llGrade.addView(view);//把这一行加到学生列表上去
+            view.setOnClickListener(new View.OnClickListener() {//设置这一行的点击事件，用于输入和更新学生分数
                 @Override
                 public void onClick(View v) {
-                    showAddDialog(studentInfo);
+                    showAddDialog(studentInfo);//弹框，把这个学生传进去
                 }
             });
         }
@@ -257,7 +258,7 @@ public class ExamInfoActivity extends BaseActivity {
         TextView btn_cancel_high_opion = view.findViewById(R.id.btn_cancel);
         TextView btn_agree_high_opion = view.findViewById(R.id.btn_confirm);
         TextView tv_title = view.findViewById(R.id.tv_title);
-        tv_title.setText(studentInfo.student_name + "同学");
+        tv_title.setText(studentInfo.student_name + "同学");//弹框上的标题表示显示 学生名字+"同学"
         btn_cancel_high_opion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -265,21 +266,21 @@ public class ExamInfoActivity extends BaseActivity {
             }
         });
 
-        if (studentInfo.scoreInfo != null) {
+        if (studentInfo.scoreInfo != null) {//如果这个学生有分数信息，就把分数显示上，即改成绩
             editText.setText(studentInfo.scoreInfo.score_num + "");
         }
         btn_agree_high_opion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(editText.getText())) {
+                if (TextUtils.isEmpty(editText.getText())) {//没写分数就提示一下
                     Toast.makeText(ExamInfoActivity.this, "请输入分数", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 try {
                     float newScore = Float.parseFloat(editText.getText().toString().trim());
                     if (studentInfo.scoreInfo != null) {
-                        studentInfo.scoreInfo.score_num = newScore;
-                    } else {
+                        studentInfo.scoreInfo.score_num = newScore;//把分数赋给这个学生
+                    } else {//如果这个学生没有分数信息，就当下创建一个空的，然后把必要信息赋上，包括分数
                         ScoreInfo scoreInfo = new ScoreInfo();
                         scoreInfo.score_num = newScore;
                         scoreInfo.student_id = studentInfo.student_id;
@@ -287,18 +288,19 @@ public class ExamInfoActivity extends BaseActivity {
                         scoreInfo.exam_id = mGradeInfo.exam_id;
                         studentInfo.scoreInfo = scoreInfo;
                     }
-                    addScore(studentInfo.scoreInfo);
+                    addScore(studentInfo.scoreInfo);//调接口，给这个学生添加分数
                 } catch (Exception e) {
                 }
-                dialog.dismiss();
+                dialog.dismiss();//点击"取消"或"确定"后弹框消失
             }
         });
-        dialog.show();
+        dialog.show();//此处弹框刚创建好，显示出来，上面是回调
         //此处设置位置窗体大小，我这里设置为了手机屏幕宽度的3/4  注意一定要在show方法调用后再写设置窗口大小的代码，否则不起效果会
         dialog.getWindow().setLayout((ScreenUtils.getScreenWidth(this) / 4 * 3), LinearLayout.LayoutParams.WRAP_CONTENT);
     }
 
     private void addScore(ScoreInfo scoreInfo) {
+        // 创建网络请求，添加学生分数
         AddScoreTask addScoreTask = new AddScoreTask(new TaskListener<HttpResult>() {
             @Override
             public void onTaskStart(TaskListener<HttpResult> listener) {
@@ -307,10 +309,10 @@ public class ExamInfoActivity extends BaseActivity {
 
             @Override
             public void onTaskComplete(TaskListener<HttpResult> listener, HttpResult result, Exception e) {
-                addScoreView();
+                addScoreView();//请求成功后，重新显示学生和分数的列表
             }
         }, HttpResult.class);
-        addScoreTask.setParam(scoreInfo);
+        addScoreTask.setParam(scoreInfo);//参数是这个分数信息
         addScoreTask.execute();
     }
 }
